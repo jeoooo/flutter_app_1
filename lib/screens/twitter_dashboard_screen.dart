f3f6d3e32cf411e1abe056f50e7510f13d56f185
jeoooo <jeocarlolubao@gmail.com>
2024-03-30 19:51:22 +0800

    Refactor bottom navigation bar in TwitterDashboardScreen

--- lib/screens/twitter_dashboard_screen.dart
+++ lib/screens/twitter_dashboard_screen.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_app_1/screens/widgets/twitter_navbar_item.dart';
 import 'widgets/news_feed.dart';
 import 'widgets/twitter_search.dart';
 
@@ -22,30 +22,10 @@ class _TwitterDashboardState extends State<TwitterDashboard> {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
         items: [
-          BottomNavigationBarItem(
-            activeIcon: SvgPicture.asset('assets/home_icon.svg',
-                color: Color(0xFF4C9EEB)),
-            icon: SvgPicture.asset('assets/home_icon.svg'),
-            // label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            activeIcon: SvgPicture.asset('assets/search_icon.svg',
-                color: Color(0xFF4C9EEB)),
-            icon: SvgPicture.asset('assets/search_icon.svg'),
-            // label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            activeIcon: SvgPicture.asset('assets/bell_icon.svg',
-                color: Color(0xFF4C9EEB)),
-            icon: SvgPicture.asset('assets/bell_icon.svg'),
-            // label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            activeIcon: SvgPicture.asset('assets/mail_icon.svg',
-                color: Color(0xFF4C9EEB)),
-            icon: SvgPicture.asset('assets/mail_icon.svg'),
-            // label: 'Profile',
-          ),
+          TwitterNavbarItem(iconPath: 'assets/home_icon.svg'),
+          TwitterNavbarItem(iconPath: 'assets/search_icon.svg'),
+          TwitterNavbarItem(iconPath: 'assets/bell_icon.svg'),
+          TwitterNavbarItem(iconPath: 'assets/mail_icon.svg'),
         ],
         currentIndex: _selectedIndex,
         onTap: (index) {
@@ -59,7 +39,6 @@ class _TwitterDashboardState extends State<TwitterDashboard> {
           case 0:
             return NewsFeed();
           case 1:
-            // Placeholder for Search tab
             return TwitterSearch();
           case 2:
             // Placeholder for Notifications tab
