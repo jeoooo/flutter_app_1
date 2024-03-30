b4beb7e0890e34c07347b4559f827d65be1992ca
jeoooo <jeocarlolubao@gmail.com>
2024-03-30 14:27:35 +0800

    Refactor TwitterDashboard screen and implement tab navigation

--- lib/screens/twitter_dashboard_screen.dart
+++ lib/screens/twitter_dashboard_screen.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, library_private_types_in_public_api, use_super_parameters, prefer_const_literals_to_create_immutables, unused_import, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, library_private_types_in_public_api, use_super_parameters, unnecessary_import
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:simple_icons/simple_icons.dart';
-
-import 'widgets/third_party_auth_login_button.dart';
+import 'widgets/tweet.dart';
 import 'widgets/twitter_navbar.dart';
 
 class TwitterDashboard extends StatefulWidget {
@@ -17,141 +14,72 @@ class TwitterDashboard extends StatefulWidget {
 }
 
 class _TwitterDashboardState extends State<TwitterDashboard> {
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      backgroundColor: Colors.white,
-      navigationBar: TwitterNavbar(),
-      child: SafeArea(
-        child: Column(
-          children: [
-            Tweet(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Tweet extends StatelessWidget {
-  const Tweet({
-    super.key,
-  });
+  int _selectedIndex = 0; // Index for the selected tab
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.grey, // Replace with desired color
-            width: 1, // Adjust the width as needed
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.bell),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
-      child: Padding(
-        padding: const EdgeInsets.only(
-            left: 8.0, right: 8.0, top: 16.0, bottom: 16.0),
-        child: Container(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                backgroundColor: Colors.blue,
-                radius: 32,
-              ),
-              SizedBox(width: 8.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      text: 'Juan Dela Cruz',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                      children: [
-                        TextSpan(
-                          text: ' ',
-                          style: TextStyle(fontSize: 18, color: Colors.grey),
-                        ),
-                        TextSpan(
-                          text: '@username',
-                          style: TextStyle(fontSize: 18, color: Colors.grey),
-                        ),
-                        TextSpan(
-                          text: ' • ',
-                          style: TextStyle(fontSize: 18, color: Colors.grey),
-                        ),
-                        TextSpan(
-                          text: '12h',
-                          style: TextStyle(fontSize: 18, color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                    child: Container(
-                      width: 300,
-                      child: Text(
-                          'datadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadata'),
-                    ),
-                  ),
-                  Row(
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return CupertinoTabView(
+              builder: (context) => CupertinoPageScaffold(
+                navigationBar: TwitterNavbar(),
+                backgroundColor: Colors.white,
+                child: SafeArea(
+                  child: Column(
                     children: [
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/comment_icon.svg',
-                            height: 20,
-                          ),
-                          SizedBox(width: 4.0),
-                          Text(
-                            '123',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(width: 32.0),
-                          SvgPicture.asset(
-                            'assets/retweet_icon.svg',
-                            height: 16,
-                          ),
-                          SizedBox(width: 4.0),
-                          Text(
-                            '123',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(width: 32.0),
-                          SvgPicture.asset(
-                            'assets/heart_icon.svg',
-                            height: 16,
-                          ),
-                          SizedBox(width: 4.0),
-                          Text(
-                            '123',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(width: 32.0),
-                          SvgPicture.asset(
-                            'assets/share_icon.svg',
-                            height: 16,
-                          ),
-                          SizedBox(width: 4.0),
-                          Text(
-                            '123',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
+                      Tweet(),
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            );
+          case 1:
+            // Placeholder for Search tab
+            return Center(
+              child: Text('Search Tab'),
+            );
+          case 2:
+            // Placeholder for Notifications tab
+            return Center(
+              child: Text('Notifications Tab'),
+            );
+          case 3:
+            // Placeholder for Profile tab
+            return Center(
+              child: Text('Profile Tab'),
+            );
+          default:
+            return Container();
+        }
+      },
     );
   }
 }
---
+++ lib/screens/widgets/tweet.dart
@@ -0,0 +1,128 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, library_private_types_in_public_api, use_super_parameters, prefer_const_literals_to_create_immutables, unused_import, sized_box_for_whitespace, unnecessary_import
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Tweet extends StatelessWidget {
+  const Tweet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey, // Replace with desired color
+            width: 1, // Adjust the width as needed
+          ),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+            left: 8.0, right: 8.0, top: 16.0, bottom: 16.0),
+        child: Container(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.blue,
+                radius: 32,
+              ),
+              SizedBox(width: 8.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: 'Juan Dela Cruz',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                          text: ' ',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        TextSpan(
+                          text: '@username',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        TextSpan(
+                          text: ' • ',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                        TextSpan(
+                          text: '12h',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                    child: Container(
+                      width: 300,
+                      child: Text(
+                          'datadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadatadata'),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/comment_icon.svg',
+                            height: 20,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text(
+                            '123',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(width: 32.0),
+                          SvgPicture.asset(
+                            'assets/retweet_icon.svg',
+                            height: 16,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text(
+                            '123',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(width: 32.0),
+                          SvgPicture.asset(
+                            'assets/heart_icon.svg',
+                            height: 16,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text(
+                            '123',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(width: 32.0),
+                          SvgPicture.asset(
+                            'assets/share_icon.svg',
+                            height: 16,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text(
+                            '123',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
